8dea4c341e4da7e529e31f10c777cf205c383b3d
erandawi <erandalakshan3567@gmail.com>
2025-03-07 23:36:09 +0530

    Data Validation Completed

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -2,7 +2,7 @@ from networksecurity.enitiy.artifact_enitiy import DataIngestionArtifact, DataVa
 from networksecurity.enitiy.config_entity import DataValidationConfig
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.constant.training_pipeline import SCHEMA_FILE_PATH
-from networksecurity.utils.main_utils.utils import read_yaml_file
+from networksecurity.utils.main_utils.utils import read_yaml_file, write_yaml_file
 
 from networksecurity.logging.logger import logging
 from scipy.stats import ks_2samp
@@ -10,7 +10,7 @@ import pandas as pd
 import os, sys
 
 class DataValidation:
-    def __inti___(self, data_ingestion_artifact: DataIngestionArtifact,
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact,
                 data_validation_config:DataValidationConfig):
         
         try:
@@ -38,8 +38,36 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
+    def detect_dataset_drift(self, base_df, current_bf, threshold=0.5) ->bool:
+        try:
+            status = True
+            report ={}
+            for column in base_df.columns:
+                d1=base_df[column]
+                d2=current_bf[column]
+                is_same_dist=ks_2samp(d1,d2)
+                if threshold <= is_same_dist.pvalue:
+                    is_found=False
+                else:
+                    is_found=True
+                    status = False
+                report.update({column:{
+                    "p_value":float(is_same_dist.pvalue),
+                    "drift_status":is_found
+                }})
+            drift_report_file_path = self.data_validation_config.drift_report_file_path
+            
+            #Create directory
+            dir_path = os.path.dirname(drift_report_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            write_yaml_file(file_path=drift_report_file_path, content = report)
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
     def initiate_data_validation(self) -> DataValidationArtifact:
         try:
+            #train_file_path = self.data_ingestion_artifact.trained_file_path
             train_file_path = self.data_ingestion_artifact.trained_file_path
             test_file_path = self.data_ingestion_artifact.test_file_path
             
@@ -54,6 +82,30 @@ class DataValidation:
             status = self.validate_number_of_columns(dataframe=test_dataframe)
             if not status:
                 error_message=f"Train dataframe does not contain  all columns. \n"
+            
+            ## lets check datadrift
+            status =self.detect_dataset_drift(base_df=train_dataframe, current_bf=test_dataframe)
+            dir_path = os.path.dirname(self.data_validation_config.valid_train_file_path)
+            os.makedirs(dir_path, exist_ok=True)
+            
+            train_dataframe.to_csv(
+                self.data_validation_config.valid_train_file_path, index = False, header=True
+            )
+            
+            test_dataframe.to_csv(
+                self.data_validation_config.valid_test_file_path, index = False, header=True
+            )
+            
+            data_validation_artifact = DataValidationArtifact(
+                validation_status = status,
+                valid_train_file_path=self.data_ingestion_artifact.trained_file_path,
+                valid_test_file_path =self.data_ingestion_artifact.test_file_path,
+                invalid_train_file_path=None,
+                invalid_test_file_path=None,
+                drift_report_file_path=self.data_validation_config.drift_report_file_path,
+            )
+            
+            return data_validation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
         
